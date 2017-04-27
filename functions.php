a70016c679c1e40ded042eef17bffad781a694c1
ad-ovo <vadiasova.olga@gmail.com>
2017-04-27 21:50:35 +0300

    Implementation of the templating

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+    function createTemplate($file_path, $data) {
+        if(!!file_exists($file_path)) {
+            foreach($data as $key) {
+                $data[$key] = htmlspecialchars($key);
+            }
+
+            ob_start();
+            extract($data);
+            include_once($file_path);
+            ob_get_flush();
+        } else {
+            return '';
+        }
+    }
+
+?>
